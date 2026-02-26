6b878c02a4dfd66db35638427b1164ee24a19ade
Hassan <h.jamil1713@gmail.com>
2026-02-27 00:46:16 +0500

    Test commit & push

--- Catalog/Models/Product.cs
+++ Catalog/Models/Product.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = default!;
+        public string ImageType { get; set; } = default!;
     }
 }
